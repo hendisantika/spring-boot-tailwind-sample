7739f26dafee32980c6e1eea4c1fb8716c791e51
hendisantika <hendisantika@yahoo.co.id>
2024-07-17 13:40:14 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springboottailwindsample/SpringBootTailwindSampleApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springboottailwindsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTailwindSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTailwindSampleApplication.class, args);
+    }
+
+}
